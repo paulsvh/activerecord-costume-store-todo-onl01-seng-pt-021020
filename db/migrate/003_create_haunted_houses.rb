b96f8a9ef81359adc89e85de145d27f15a2e5d45
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-22 01:26:27 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,7 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
 
   def change
-    create_table :costume_stores do |t|
+    create_table :haunted_houses do |t|
       t.string :name
       t.string :location
       t.string :theme
